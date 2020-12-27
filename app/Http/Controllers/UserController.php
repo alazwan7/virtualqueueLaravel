f4d52e6a271516c1d5e366991ad38c027f125f14
alazwan7 <alazwan7@gmail.com>
2020-12-27 23:47:41 +0800

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+      $user = Users::All();
+
+
+
+      return $user;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    //insert
+    public function store(Request $request)
+    {
+
+        $user = new Users;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $hashed = Hash::make($request->password, [
+            'rounds' => 12,
+        ]);
+        $user->password =$hashed;
+        $user->role = $request->role;
+        $user->fullname = $request->fullname;
+        $user->icno = $request->icno;
+        $user->phoneno = $request->phoneno;
+
+        $user->save();
+        return($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Users::find($id);
+
+        return $user;
+
+
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+         $user = Users::find($id);
+
+         $user->name= $request->name;
+         $user->email=$request->email;
+         $hashed = Hash::make($request->password, [
+            'rounds' => 12,
+        ]);
+         $user->password =$hashed;
+         $user->fullname=$request->fullname;
+         $user->phoneno=$request->phoneno;
+
+
+         $user->save();
+
+         return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = App\Models\Users::find(1);
+
+        $user->delete();
+    }
+
+    public function authenticate(Request $request)
+    {
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return ["user"=> $user,'message' => "success",'role' => $user->role];
+        }
+        else{
+
+            return "faillll";
+        }
+    }
+}
+
+
+
+
+
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/users', function () {
+//     return new UsersResource(users::find(1));
+// });
+Route::get('/users', "App\Http\Controllers\userController@index");
+Route::get('/users/{id}', "App\Http\Controllers\userController@show");
+Route::post('/users', "App\Http\Controllers\userController@store");
+Route::put('/users/{id}', "App\Http\Controllers\userController@update");
+Route::post('/login', "App\Http\Controllers\userController@authenticate");
+// Route::get('/login', "App\Http\Controllers\userController@authenticate");
