22a321408297e095ee84242f6cd926bbfbde3c6b
alazwan7 <alazwan7@gmail.com>
2020-12-28 00:02:15 +0800

    add foreign key table

---
+++ database/migrations/2020_12_27_155328_create_request_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->id();
+            $table->string('age');
+            $table->string('ability');
+            $table->string('currentLocation');
+            $table->timestamps();
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+}
